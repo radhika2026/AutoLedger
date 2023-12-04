bf0a2c9f1bb0ac0b2de6e9ca1180a3f133cf462a
xf4c70r <arvindsudarshan18@gmail.com>
2023-12-04 08:25:41 -0800

    made some changes to service center form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import "./ServiceCenterForm.css";
 import HomeComponent from './HomeComponent';
 import VehicleInfo from './VehicleInfoPage';
 import NavbarComponent from './NavbarComponent';
 import SignInModal from './SignInModal';
 import RegistrationModal from './RegistrationModal';
+import ServiceCenterForm from './ServiceCenterForm';
 import DMV from './DMV';
 
 function App() {
@@ -27,6 +29,8 @@ function App() {
       <Routes>
         <Route path="/home" element={<HomeComponent />} />
         <Route path="/vehicleinfopage" element={<VehicleInfo />} />
+        <Route path="/Serv" element={<VehicleInfo />} />
+        <Route path="/ServiceCenterForm" element={<ServiceCenterForm />} />
         <Route path="/dmv" element={<DMV />} />
         {/* Define other routes as needed */}
       </Routes>
