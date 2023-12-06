ffa3e92783dbd16debabc243b47cf572654e4f26
disha <dishan.cs19@bmsce.ac.in>
2023-12-05 19:40:15 -0800

    insurancepage_updated

--- frontend/src/InsuranceDropdown.js
+++ frontend/src/InsuranceDropdown.js
@@ -184,192 +184,48 @@ const InsuranceDropdown = () => {
                                 ) : (
                                     <></>
                                 )}
-                            </Form.Group>
+                         
                             <Form.Group className="mb-3">
-                                <Form.Label>Owner Name:</Form.Label>
-                                <Input type="text" name="ownerName" value={carDetails.ownerName} onChange={handleCarDetailsChange} />
-                                {carDetails.ownerName.length > 25 ? (
-                                    <Form.Text className="text-danger">Owner name cannot exceed 25 characters.</Form.Text>
-                                ) : (
-                                    <></>
-                                )}
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Driving License Number:</Form.Label>
+                                <Form.Label>Insurance Number:</Form.Label>
                                 <Input type="text" name="drivingLicenseNumber" value={carDetails.drivingLicenseNumber} onChange={handleCarDetailsChange} />
                                 {carDetails.drivingLicenseNumber.length < 8 || carDetails.drivingLicenseNumber.length > 10 ? (
-                                    <Form.Text className="text-danger">Driving license number must be 8 to 10 characters long.</Form.Text>
+                                    <Form.Text className="text-danger">Insurance number must be 8 to 10 characters long.</Form.Text>
                                 ) : (
                                     <></>
                                 )}
                             </Form.Group>
+
+                            </Form.Group>
                             <Form.Group className="mb-3">
-                                <Form.Label>Chasis Number:</Form.Label>
+                                <Form.Label>Incident</Form.Label>
+                                <Input type="text" name="ownerName" value={carDetails.ownerName} onChange={handleCarDetailsChange} />
+                              
+                            </Form.Group>
+
+                            <Form.Group className="mb-3">
+                                <Form.Label>Damage</Form.Label>
+                                <Input type="text" name="ownerName" value={carDetails.ownerName} onChange={handleCarDetailsChange} />
+                              
+                            </Form.Group>
+
+                            <Form.Group className="mb-3">
+                                <Form.Label>Service Centre id:</Form.Label>
                                 <Input type="text" name="chasisNumber" value={carDetails.chasisNumber} onChange={handleCarDetailsChange} />
-                                {carDetails.chasisNumber.length !== 17 ? (
-                                    <Form.Text className="text-danger">Chassis number must be 17 characters long.</Form.Text>
-                                ) : (
-                                    <></>
-                                )}
+                            
                             </Form.Group>
 
-                            <Form.Group className="mb-3">
-                                <Form.Label>Engine Number:</Form.Label>
-                                <Input type="text" name="engineNumber" value={carDetails.engineNumber} onChange={handleCarDetailsChange} />
-                                {carDetails.engineNumber.length < 11 || carDetails.engineNumber.length > 17 ? (
-                                    <Form.Text className="text-danger">Engine number must be 11 to 17 characters long.</Form.Text>
-                                ) : (
-                                    <></>
-                                )}
-                            </Form.Group>
+        
 
                             <Form.Group className="mb-3">
-                                <Form.Label>Manufacturing Date:</Form.Label>
+                                <Form.Label> Date:</Form.Label>
                                 <Input type="date" name="manufacturingDate" value={carDetails.manufacturingDate} onChange={handleCarDetailsChange} />
                             </Form.Group>
 
-                            <Form.Group className="mb-3">
-                                <Form.Label>Odometer Reading:</Form.Label>
-                                <Input type="number" name="odometerReading" value={carDetails.odometerReading} onChange={handleCarDetailsChange} />
-                            </Form.Group>
 
-                            <Form.Group className="mb-3">
-                                <Form.Label>Manufacturer:</Form.Label>
-                                <Input type="text" name="manufacturer" value={carDetails.manufacturer} onChange={handleCarDetailsChange} required />
-                                {carDetails.manufacturer.length > 25 ? (
-                                    <Form.Text className="text-danger">Manufacturer name cannot exceed 25 characters.</Form.Text>
-                                ) : (
-                                    <></>
-                                )}
-                            </Form.Group>
+
 
                             {/* <Button variant="primary" onClick={handleNext}>Next</Button> */}
-                            <h2>Car Features</h2>
 
-                            <Form.Group controlId="color">
-                                <Form.Label>Color:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    name="color"
-                                    value={carFeatures.color}
-                                    onChange={handleCarFeaturesChange}
-                                />
-                                {carFeatures.color.length > 25 ? (
-                                    <Form.Text className="text-danger">Color name cannot exceed 25 characters.</Form.Text>
-                                ) : (
-                                    <></>
-                                )}
-                            </Form.Group>
-
-                            <Form.Group controlId="seating">
-                                <Form.Label>Seating Capacity:</Form.Label>
-                                <Form.Control
-                                    type="number"
-                                    name="seating"
-                                    value={carFeatures.seating}
-                                    onChange={handleCarFeaturesChange}
-                                />
-                            </Form.Group>
-
-
-                            <Form.Group controlId="transmission">
-                                <Form.Label>Transmission:</Form.Label>
-                                <FormControl
-                                    as="select"
-                                    name="transmission"
-                                    value={carFeatures.transmission}
-                                    onChange={handleCarFeaturesChange}
-                                >
-                                    <option value="">Select an option</option>
-                                    <option value="Automatic">Automatic</option>
-                                    <option value="Manual">Manual</option>
-                                </FormControl>
-                            </Form.Group>
-
-
-
-                            <Form.Group controlId="wheelBase">
-                                <Form.Label>Wheel Base (in meters):</Form.Label>
-                                <Form.Control
-                                    type="number"
-                                    name="wheelBase"
-                                    step="0.01"
-                                    value={carFeatures.wheelBase}
-                                    onChange={handleCarFeaturesChange}
-                                />
-                            </Form.Group>
-
-                            <Form.Group controlId="driveType">
-                                <Form.Label>Drive Type:</Form.Label>
-                                <FormControl
-                                    as="select"
-                                    name="driveType"
-                                    value={carFeatures.driveType}
-                                    onChange={handleCarFeaturesChange}
-                                >
-                                    <option value="">Select an option</option>
-                                    <option value="Front Wheel">Front Wheel</option>
-                                    <option value="Rear Wheel">Rear Wheel</option>
-                                    <option value="Four Wheel">Four Wheel</option>
-                                </FormControl>
-                            </Form.Group>
-
-                            <Form.Group controlId="groundClearance">
-                                <Form.Label>Ground Clearance (in meters):</Form.Label>
-                                <Form.Control type="number"
-                                    name="groundClearance"
-                                    step="0.01"
-                                    value={carFeatures.groundClearance}
-                                    onChange={handleCarFeaturesChange}
-                                />
-                            </Form.Group>
-
-
-                            <Form.Group controlId="fuel">
-                                <Form.Label>Fuel:</Form.Label>
-                                <FormControl
-                                    as="select"
-                                    name="fuel"
-                                    value={carFeatures.fuel}
-                                    onChange={handleCarFeaturesChange}
-                                >
-                                    <option value="">Select an option</option>
-                                    <option value="Petrol">Petrol</option>
-                                    <option value="Diesel">Diesel</option>
-                                    <option value="Electric">Electric</option>
-                                </FormControl>
-                            </Form.Group>
-
-                            <Form.Group controlId="class">
-                                <Form.Label>Class:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    name="class"
-                                    value={carFeatures.class}
-                                    onChange={handleCarFeaturesChange}
-                                />
-
-                                {carFeatures.class.length > 25 ? (
-                                    <Form.Text className="text-danger">Class name cannot exceed 25 characters.</Form.Text>
-                                ) : (
-                                    <></>
-                                )}
-                            </Form.Group>
-
-                            <Form.Group controlId="model">
-                                <Form.Label>Model:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    name="model"
-                                    value={carFeatures.model}
-                                    onChange={handleCarFeaturesChange}
-                                />
-                                {carFeatures.model.length > 25 ? (
-                                    <Form.Text className="text-danger">Model name cannot exceed 25 characters.</Form.Text>
-                                ) : (
-                                    <></>
-                                )}
-                            </Form.Group>
                             <Button variant="primary" onClick={handleSubmit}>Submit</Button>
 
                         </>
