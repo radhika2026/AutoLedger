d886a9523f4a5a5281df2437c1cc8e995c3560d5
disha <dishan.cs19@bmsce.ac.in>
2023-12-06 19:21:36 -0800

    footer_basic

---
+++ frontend/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2023 AutoLedger. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
